55281a8bfe503c501b51b8aa38c8fda76dda62d9
tiago <tiagobrunobfr@gmail.com>
2020-07-21 17:35:01 -0300

    testing class IPFizzBuzzTest

--- tests/IPFizzBuzzTest.php
+++ tests/IPFizzBuzzTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Exception\InvalidIP4Format;
 use PHPUnit\Framework\TestCase;
 
 class IPFizzBuzzTest extends TestCase
@@ -15,9 +16,43 @@ class IPFizzBuzzTest extends TestCase
 
     /**
      * @test
+     * @dataProvider ExcepitionProvider
      */
-    public function shouldReturnFizz()
+    public function shouldReturnExceptionInvalidIP4($value){
+
+        $this->expectException(InvalidIP4Format::class);
+
+        $this->ipFizzBuzz->getFizzBuzzByIP($value);
+
+    }
+
+    public function ExcepitionProvider()
     {
-        $this->assertEquals("Fizz", $this->ipFizzBuzz->getFizzBuzzByIP("127.0.0.3"));
+        return [
+            'WhenPartsIsDifferentOf4' => array('127.0.0'),
+            'WhenPartsIsNotNumericInString' => array('aaa.b.c.d'),
+            'WhenPartsIsNotString' => array(111111)
+        ];
+
+    }
+
+    /**
+     * @test
+     * @dataProvider providerSuccessIp
+     */
+    public function shouldBeReturnValid($expected, $value)
+    {
+
+        $this->assertEquals($expected, $this->ipFizzBuzz->getFizzBuzzByIP($value));
+
+    }
+
+    public function providerSuccessIp()
+    {
+        return [
+            'WhenTheValueIsMultipleOf3' => array('expected' => 'Fizz', 'value' => '127.0.0.3'),
+            'WhenTheValueIsMultipleOf5' => array('expected' => 'Buzz', 'value' => '127.0.0.5'),
+            'WhenTheValueIsNotMultipleOf3AndOf5' => array('expected' => '1', 'value' => '127.0.0.1'),
+        ];
     }
 }
